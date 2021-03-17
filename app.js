2eae475329251809c74202148161da8d4a4a0da9
arunLogan <arun.11143@gmail.com>
2021-03-17 13:29:17 +0530

    refresh commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 var express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const HttpError = require("./models/http-error");
+const sgMail = require("@sendgrid/mail");
+// console.log(process.env.SENDGRID_API_KEY);
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
@@ -29,6 +32,26 @@ app.use((req, res, next) => {
   next();
 });
 // app.use(cors());
+app.get("/checkmail", (req, res) => {
+  // console.log("this is key", process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "arun.11143@gmail.com", // Change to your recipient
+    from: "arun@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then((data) => {
+      console.log("Email sent", data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  res.send("success");
+});
 app.use(mainRoutes);
 app.use(userRoutes);
 app.use(formRoutes);
